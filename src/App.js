6bc3b5b4278122982787f50c9f40f73770b4e20f
Maya Jerndahl <144351614+MjBonk@users.noreply.github.com>
2024-01-30 11:42:10 +0100

    remove context provider

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import { useContext } from "react";
 import "./App.css";
 import Button from "./components/Button/Button";
 import ColorBox from "./components/ColorBox/ColorBox";
 import Header from "./components/Header/Header";
-import ContextProvider from "./ContextProvider";
 
 function App() {
-
 	return (
-		<ContextProvider.Provider>
-			<main>
-				<Header />
-				<Button />
-				<section id="section1" className="site-section section1">
-					<div className="text-wrapper">
-						<h1>I'M A TITLE</h1>
-						<p>
-							Emmet is great for that. With it installed in the code editor
-							you are using, you can type “lorem” and then tab and it will
-							expand into a paragraph of Lorem Ipsum placeholder text. But it
-							can do more! You can control how much you get, place it within
-							HTML structure as it expands, and get different bits of it in
-							repeated elements.
-						</p>
-					</div>
-				</section>
-				<section id="section2" className="site-section section2">
-					<ColorBox setColor={setColor1} backgroundColor={color1} />
-					<ColorBox  />
-				</section>
+		<main>
+			<Header />
+			<Button />
+			<section id="section1" className="site-section section1">
+				<div className="text-wrapper">
+					<h1>I'M A TITLE</h1>
+					<p>
+						Emmet is great for that. With it installed in the code editor you are
+						using, you can type “lorem” and then tab and it will expand into a
+						paragraph of Lorem Ipsum placeholder text. But it can do more! You can
+						control how much you get, place it within HTML structure as it expands,
+						and get different bits of it in repeated elements.
+					</p>
+				</div>
+			</section>
+			<section id="section2" className="site-section section2">
+				<ColorBox />
+				<ColorBox />
+			</section>
 
-				<section id="section3" className="site-section section3">
-					3
-				</section>
-			</main>
-		</ContextProvider.Provider>
+			<section id="section3" className="site-section section3">
+				3
+			</section>
+		</main>
 	);
 }
 
--- src/components/ColorBox/ColorBox.js
+++ src/components/ColorBox/ColorBox.js
@@ -1,15 +1,17 @@
 import "./ColorBox.css";
 import { HuePicker } from "react-color";
+import { useState } from "react";
 
 function ColorBox() {
+	const [colors, setColors] = useState("");
 
 	function handleOnChange(color) {
-		console.log(color.hex);
+		setColors(color.hex);
 	}
 
 	return (
 		<div className="color-box">
-			<HuePicker />
+			<HuePicker color={colors} onChange={handleOnChange} />
 			{/* <SliderPicker /> */}
 		</div>
 	);
